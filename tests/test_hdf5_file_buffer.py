e339a59b4a99b4ef24992efa191549352ab18f52
ranjanl <ranjan.lamichhane@sambanovasystems.com>
2023-06-15 17:09:15 -0700

    improved marked pyteset

--- tests/test_hdf5_file_buffer.py
+++ tests/test_hdf5_file_buffer.py
@@ -14,59 +14,149 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import os
+import tempfile
+
 import h5py
 import numpy as np
-import os
 import pytest
-import tempfile
 
 from generative_data_prep.data_buffers import Hdf5FileBuffer
 from generative_data_prep.tokenized_line import TokenizedSequence
 
 MAX_SEQ_LENGTH = 4
-DATA_TYPE = 'i4'
+DATA_TYPE = "i4"
 CHUNK_SIZE_1 = np.dtype(DATA_TYPE).itemsize * MAX_SEQ_LENGTH
 CHUNK_SIZE_2 = CHUNK_SIZE_1 * 2
 CHUNK_SIZE_3 = CHUNK_SIZE_1 * 3
 EOS = 9
 
 
+@pytest.mark.fast
 @pytest.mark.parametrize(
-    'max_seq_length1,length,max_seq_length,eos_token_id,chunk_size,num_iterations,gold_input_ids,gold_token_type_ids',
-    [(MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_1, 1, np.array([[0, 1, 2, 3]
-                                                                                      ]), np.array([[0, -1, -2, -3]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_2, 1, np.array([[0, 1, 2, 3]
-                                                                                      ]), np.array([[0, -1, -2, -3]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_3, 1, np.array([[0, 1, 2, 3]
-                                                                                      ]), np.array([[0, -1, -2, -3]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_1, 2, np.array(
-         [[0, 1, 2, 3], [1, 2, 3, 4]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_2, 2, np.array(
-         [[0, 1, 2, 3], [1, 2, 3, 4]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_3, 2, np.array(
-         [[0, 1, 2, 3], [1, 2, 3, 4]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_1, 3,
-      np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_2, 3,
-      np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]])),
-     (MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, MAX_SEQ_LENGTH, EOS, CHUNK_SIZE_3, 3,
-      np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]), np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]]))]
+    "max_seq_length1,length,max_seq_length,eos_token_id,chunk_size,num_iterations,gold_input_ids,gold_token_type_ids",
+    [
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_1,
+            1,
+            np.array([[0, 1, 2, 3]]),
+            np.array([[0, -1, -2, -3]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_2,
+            1,
+            np.array([[0, 1, 2, 3]]),
+            np.array([[0, -1, -2, -3]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_3,
+            1,
+            np.array([[0, 1, 2, 3]]),
+            np.array([[0, -1, -2, -3]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_1,
+            2,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_2,
+            2,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_3,
+            2,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_1,
+            3,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_2,
+            3,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]]),
+        ),
+        (
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            MAX_SEQ_LENGTH,
+            EOS,
+            CHUNK_SIZE_3,
+            3,
+            np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]),
+            np.array([[0, -1, -2, -3], [1, 0, -1, -2], [2, 1, 0, -1]]),
+        ),
+    ],
 )
-def test_hfd5_text_buffer_write(max_seq_length1: int, tokenized_line: TokenizedSequence, chunk_size: int,
-                                num_iterations: int, gold_input_ids: np.ndarray, gold_token_type_ids: np.ndarray):
+def test_hfd5_text_buffer_write(
+    max_seq_length1: int,
+    tokenized_line: TokenizedSequence,
+    chunk_size: int,
+    num_iterations: int,
+    gold_input_ids: np.ndarray,
+    gold_token_type_ids: np.ndarray,
+):
     with tempfile.TemporaryDirectory() as output_dir:
-        hdf5_file_path = os.path.join(output_dir, 'temp.hdf5')
-        with Hdf5FileBuffer(hdf5_file_path, max_seq_length1, DATA_TYPE, chunk_size) as f:
+        hdf5_file_path = os.path.join(output_dir, "temp.hdf5")
+        with Hdf5FileBuffer(
+            hdf5_file_path, max_seq_length1, DATA_TYPE, chunk_size
+        ) as f:
             for i in range(num_iterations):
                 tokenized_line_copy = tokenized_line[:]
-                tokenized_line_copy._token_ids = list(map(lambda x: x + i, tokenized_line_copy._token_ids))
-                tokenized_line_copy._token_type_ids = list(map(lambda x: x + i, tokenized_line_copy._token_type_ids))
+                tokenized_line_copy._token_ids = list(
+                    map(lambda x: x + i, tokenized_line_copy._token_ids)
+                )
+                tokenized_line_copy._token_type_ids = list(
+                    map(lambda x: x + i, tokenized_line_copy._token_type_ids)
+                )
                 f.write([tokenized_line_copy])
-        with h5py.File(hdf5_file_path, 'r') as f:
+        with h5py.File(hdf5_file_path, "r") as f:
             assert str(f.keys()) == "<KeysViewHDF5 ['input_ids', 'token_type_ids']>"
-            assert f['input_ids'].shape == gold_input_ids.shape
-            for input_ids_i, gold_input_ids_i in zip(f['input_ids'], gold_input_ids):
+            assert f["input_ids"].shape == gold_input_ids.shape
+            for input_ids_i, gold_input_ids_i in zip(f["input_ids"], gold_input_ids):
                 assert all(input_ids_i == gold_input_ids_i)
-            assert f['token_type_ids'].shape == gold_token_type_ids.shape
-            for token_type_ids_i, gold_token_type_ids_i in zip(f['token_type_ids'], gold_token_type_ids):
+            assert f["token_type_ids"].shape == gold_token_type_ids.shape
+            for token_type_ids_i, gold_token_type_ids_i in zip(
+                f["token_type_ids"], gold_token_type_ids
+            ):
                 assert all(token_type_ids_i == gold_token_type_ids_i)
