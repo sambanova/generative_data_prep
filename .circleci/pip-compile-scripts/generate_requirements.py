83f99dae874561e237a97ec44c9db1ea0998cd31
ranjanl <ranjan.lamichhane@sambanovasystems.com>
2023-07-06 16:16:43 -0700

    rename requirements

---
+++ .circleci/pip-compile-scripts/generate_requirements.py
@@ -0,0 +1,54 @@
+"""This script generates requirements files that will be executed by pre-commit as a hook and on CI."""
+
+import argparse
+import os
+import re
+import shlex
+import subprocess  # nosec
+
+
+def main():
+    """The main function."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--python-version", help="python version", required=True)
+    parser.add_argument("--options", help="list of dependencies to build", required=True)
+    args = parser.parse_args()
+
+    # Requirements Folders
+    req_folder = "requirements"
+
+    # Retrieve pip and pip-tools versions from the environment
+    pip_op = subprocess.run(["pip", "show", "pip"], capture_output=True, check=True)  # nosec
+    pip_tools_op = subprocess.run(["pip", "show", "pip-tools"], capture_output=True, check=True)  # nosec
+    pip_version = re.search("Version: (.+)", pip_op.stdout.decode("utf-8")).group(1)
+    pip_tools_version = re.search("Version: (.+)", pip_tools_op.stdout.decode("utf-8")).group(1)
+
+    # Identify mount paths for docker command
+    pwd = os.getcwd()
+
+    # pip-compile common options
+    pc_opts = "pip-compile --resolver=backtracking --allow-unsafe pyproject.toml --strip-extras"
+
+    pip_compile_cmds = [f"{pc_opts} --output-file {req_folder}/requirements.txt"]
+    dependency_options = args.options.split(",")
+    for dep in dependency_options:
+        pip_compile_cmds.append(f"{pc_opts} --extra={dep} --output-file {req_folder}/requirements-{dep}.txt")
+
+    # Construct pip-compile commands
+    pip_cmd = f"pip install pip=={pip_version} && pip install pip-tools=={pip_tools_version}"
+
+    combo_pip_compile_cmd = f'bash -c \'{" && ".join([pip_cmd] + pip_compile_cmds)}\''
+    docker_command = f"docker run --rm -v {pwd}:/app -w /app \
+        python:{args.python_version} {combo_pip_compile_cmd}"
+
+    # Run pip-compile commands according to environment
+    if os.getenv("CIRCLECI") == "true":
+        pre_commit_command = combo_pip_compile_cmd
+    else:
+        pre_commit_command = docker_command
+
+    subprocess.run(shlex.split(pre_commit_command), check=True)  # nosec
+
+
+if __name__ == "__main__":
+    main()
