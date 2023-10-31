097511a2a4180319d6fb6b0fc94d411cafcf9f65
ranjanl <ranjan.lamichhane@sambanovasystems.com>
2023-10-30 18:13:27 -0700

    test python command component, work in progress

---
+++ tests/component/test_running_command.py
@@ -0,0 +1,25 @@
+import secrets
+import subprocess
+
+
+def test_pipeline_runs_successfully():
+    """
+    GIVEN a test command to run the generative_data_prep pipeline
+    WHEN the command is executed using python
+    THEN the pipeline runs successfully and returns a return code of 0 within specified timeout
+    """
+    time_limit_seconds = 9
+    input_file_path = "tests/examples/generative_tuning/example_generative_tuning_data.jsonl"
+    input_file_path = "/Users/ranjanl/github/generative_data_prep/tests/examples/ \
+        generative_tuning/example_generative_tuning_data.jsonl"
+    output_path = f"tests/examples/tester_{secrets.token_hex(8)}"
+    command = f"python -m generative_data_prep pipeline --input_file_path={input_file_path} \
+        --output_path={output_path} --max_seq_length=1024"
+
+    try:
+        result = subprocess.run(command.split(), check=True, timeout=time_limit_seconds)
+    except subprocess.TimeoutExpired as e:
+        assert False, f"Command timed out after {time_limit_seconds} seconds. Exception: {e}"
+    except Exception as e:
+        assert False, f"Command failed with exception: {e}"
+    assert result.returncode == 0, f"Command failed with return code {result.returncode}"
