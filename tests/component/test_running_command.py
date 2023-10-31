238866a34068556dcd7448a89419131f381785c2
ranjanl <ranjan.lamichhane@sambanovasystems.com>
2023-10-31 11:46:45 -0700

    fix tests

--- tests/component/test_running_command.py
+++ tests/component/test_running_command.py
@@ -2,19 +2,18 @@ import secrets
 import subprocess
 
 
-def test_pipeline_runs_successfully():
+def test_pipeline_runs_successfully(pytestconfig):
     """
     GIVEN a test command to run the generative_data_prep pipeline
     WHEN the command is executed using python
     THEN the pipeline runs successfully and returns a return code of 0 within specified timeout
     """
     time_limit_seconds = 9
-    input_file_path = "tests/examples/generative_tuning/example_generative_tuning_data.jsonl"
-    input_file_path = "/Users/ranjanl/github/generative_data_prep/tests/examples/ \
-        generative_tuning/example_generative_tuning_data.jsonl"
-    output_path = f"tests/examples/tester_{secrets.token_hex(8)}"
-    command = f"python -m generative_data_prep pipeline --input_file_path={input_file_path} \
-        --output_path={output_path} --max_seq_length=1024"
+    root_dir = pytestconfig.rootdir
+    input_file_path = root_dir + "/tests/examples/generative_tuning/example_generative_tuning_data.jsonl"
+    output_path = f"{root_dir}/tests/examples/tester_{secrets.token_hex(8)}"
+    command = "python -m generative_data_prep pipeline "
+    command += f"--input_file_path={input_file_path} --output_path={output_path} --max_seq_length=1024"
 
     try:
         result = subprocess.run(command.split(), check=True, timeout=time_limit_seconds)
