41fc345fdaf4a0dd77260d23839ebac34de995a5
ranjanl <ranjan.lamichhane@sambanovasystems.com>
2023-10-31 12:44:53 -0700

    run tests in both python-root and current-dir

--- tests/component/test_running_command.py
+++ tests/component/test_running_command.py
@@ -1,17 +1,27 @@
+import os
 import secrets
 import subprocess
+from pathlib import Path
+
+import pytest
+
+PYTHON_ROOT_DIR = str(Path(__file__).parent.parent.parent.absolute())
+CURRENT_DIR = str(Path(__file__).parent.absolute())
 
 
-def test_pipeline_runs_successfully(pytestconfig):
+@pytest.mark.parametrize("run_path", [PYTHON_ROOT_DIR, CURRENT_DIR], ids=["python_rootdir", "home_dir"])
+def test_pipeline_runs_successfully(run_path):
     """
     GIVEN a test command to run the generative_data_prep pipeline
-    WHEN the command is executed using python
-    THEN the pipeline runs successfully and returns a return code of 0 within specified timeout
+    WHEN the command is executed in Python Root Directory
+        And the command is executed in the Current Directory
+    THEN the pipeline runs successfully
+        And returns a return code of 0 within specified timeout
     """
     time_limit_seconds = 9
-    root_dir = pytestconfig.rootdir
-    input_file_path = root_dir + "/tests/examples/generative_tuning/example_generative_tuning_data.jsonl"
-    output_path = f"{root_dir}/tests/examples/tester_{secrets.token_hex(8)}"
+    input_file_path = PYTHON_ROOT_DIR + "/tests/examples/generative_tuning/example_generative_tuning_data.jsonl"
+    output_path = f"{PYTHON_ROOT_DIR}/tests/examples/tester_{secrets.token_hex(8)}"
+    os.chdir(run_path)
     command = "python -m generative_data_prep pipeline "
     command += f"--input_file_path={input_file_path} --output_path={output_path} --max_seq_length=1024"
 
