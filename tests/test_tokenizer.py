b253ea331ba6f9f43953fcaf3e08e4cda2de54dc
Jay Rainton <jay.rainton@sambanovasystems.com>
2023-11-06 14:47:32 -0800

    fixing some small changes

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,18 +1,37 @@
 import os
 import pathlib
 
-from transformers import GPT2Config, GPT2Tokenizer, GPT2TokenizerFast
+from transformers import (
+    BertConfig,
+    BertTokenizerFast,
+    GPT2Config,
+    GPT2Tokenizer,
+    GPT2TokenizerFast,
+)
 
 from generative_data_prep.__main__ import get_tokenizer
 
 
-def test_pretrained_tokenizer():
+def test_pretrained_tokenizer_gpt2():
     """Test the tokenizer function using a pretrained_tokenizer."""
     tokenizer, model_config = get_tokenizer(
         pretrained_tokenizer="gpt2", tokenizer_class=None, vocab_file=None, merges_file=None, special_tokens_dict=None
     )
     assert type(tokenizer) == GPT2TokenizerFast
     assert type(model_config) == GPT2Config
+
+
+def test_pretrained_tokenizer_bert_base_uncased():
+    """Test the tokenizer function using a pretrained_tokenizer."""
+    tokenizer, model_config = get_tokenizer(
+        pretrained_tokenizer="bert-base-uncased",
+        tokenizer_class=None,
+        vocab_file=None,
+        merges_file=None,
+        special_tokens_dict=None,
+    )
+    assert type(tokenizer) == BertTokenizerFast
+    assert type(model_config) == BertConfig
 
 
 def test_tokenizer_class():
