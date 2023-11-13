1ecb5f78ff40ac7520e14560625e2cc1710283a5
Jay Rainton <jay.rainton@sambanovasystems.com>
2023-11-13 15:08:10 -0800

    adding in two new pytests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -88,6 +88,69 @@ def test_pydantic_model_greater_world_size():
             assert "100" in error["msg"]
             assert "4" in error["msg"]
             assert "greater than" in error["msg"]
+            assert "specified number of files" in error["msg"]
+        else:
+            assert False
+        return
+    assert False
+
+
+def test_pydantic_model_different_sequence_length():
+    """Testing DatasetMetadata loads in variables correctly. This should fail"""
+    output_dir = os.path.join(
+        Path.cwd(), "tests", "examples", "pretraining_sha256_split", "pipelined_pretraining_sha256_split"
+    )
+    metadata_file = os.path.join(output_dir, "metadata.yaml")
+    with open(metadata_file, "r") as file:
+        metadata_dict = yaml.safe_load(file)
+    context_dict = {
+        "eval": False,
+        "batch_size": 1,
+        "model_type": str(type(GPT2Config.from_pretrained("gpt2"))),
+        "vocab_size": 50257,
+        "world_size": 4,
+        "max_seq_length": 2048,
+    }
+    try:
+        DatasetMetadata.model_validate(metadata_dict, context=context_dict)
+    except ValidationError as exc:
+        assert len(exc.errors()) == 1
+        error = exc.errors()[0]
+        if "number_of_training_files" == error["loc"][0]:
+            assert "2048" in error["msg"]
+            assert "1024" in error["msg"]
+            assert "must match max_seq_length" in error["msg"]
+        else:
+            assert False
+        return
+    assert False
+
+
+def test_pydantic_model_greater_batch_size():
+    """Testing DatasetMetadata loads in variables correctly. This should fail"""
+    output_dir = os.path.join(
+        Path.cwd(), "tests", "examples", "pretraining_sha256_split", "pipelined_pretraining_sha256_split"
+    )
+    metadata_file = os.path.join(output_dir, "metadata.yaml")
+    with open(metadata_file, "r") as file:
+        metadata_dict = yaml.safe_load(file)
+    context_dict = {
+        "eval": False,
+        "batch_size": 30,
+        "model_type": str(type(GPT2Config.from_pretrained("gpt2"))),
+        "vocab_size": 50257,
+        "world_size": 4,
+        "max_seq_length": 1024,
+    }
+    try:
+        DatasetMetadata.model_validate(metadata_dict, context=context_dict)
+    except ValidationError as exc:
+        assert len(exc.errors()) == 1
+        error = exc.errors()[0]
+        if "number_of_training_files" == error["loc"][0]:
+            assert "30" in error["msg"]
+            assert "1024" in error["msg"]
+            assert "must match max_seq_length" in error["msg"]
         else:
             assert False
         return
