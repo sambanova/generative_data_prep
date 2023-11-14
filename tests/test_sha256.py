e24ad14a65611b40bdd3dec005d559df49dbb589
Jay Rainton <jay.rainton@sambanovasystems.com>
2023-11-13 17:00:05 -0800

    adding in test so it remakes sha256

--- tests/test_sha256.py
+++ tests/test_sha256.py
@@ -1,10 +1,32 @@
 import os
 from pathlib import Path
 
+import pytest
+
 from generative_data_prep.utils import validate_sha256
 
 
 def test_validation_sha_with_split():
+    """Testing validation method and sha256 created correctly. With splits."""
+    validate_dir = os.path.join(
+        Path.cwd(), "tests", "examples", "pretraining_sha256_split", "pipelined_pretraining_sha256_split"
+    )
+    # breakpoint()
+    assert validate_sha256(validate_dir)
+
+
+def fake_getsize(file_path):
+    """Fake get size function for monkeypatch."""
+    return 0
+
+
+@pytest.fixture
+def mock_getsize(monkeypatch):
+    """Mocking size function using monkeypatch."""
+    monkeypatch.setattr(os.path, "getsize", fake_getsize)
+
+
+def test_validation_sha_with_split_redoing_sha256(mock_getsize):
     """Testing validation method and sha256 created correctly. With splits."""
     validate_dir = os.path.join(
         Path.cwd(), "tests", "examples", "pretraining_sha256_split", "pipelined_pretraining_sha256_split"
