b91a17ad2950b9eb8ab3c907c2594ccfa0f813ee
Jay Rainton <jay.rainton@sambanovasystems.com>
2023-11-13 16:08:15 -0800

    adding in new test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -184,3 +184,22 @@ def test_pydantic_model_no_evaluation_files():
             ) in error["msg"]
         return
     assert False
+
+
+def test_pydantic_model_yes_evaluation_files():
+    """Testing DatasetMetadata loads in variables correctly. This should fail"""
+    output_dir = os.path.join(
+        Path.cwd(), "tests", "examples", "pretraining_sha256_split", "pipelined_pretraining_sha256_split_and_eval"
+    )
+    metadata_file = os.path.join(output_dir, "metadata.yaml")
+    with open(metadata_file, "r") as file:
+        metadata_dict = yaml.safe_load(file)
+    context_dict = {
+        "eval": True,
+        "batch_size": 1,
+        "model_type": str(type(GPT2Config.from_pretrained("gpt2"))),
+        "vocab_size": 50257,
+        "world_size": 4,
+        "max_seq_length": 1024,
+    }
+    DatasetMetadata.model_validate(metadata_dict, context=context_dict)
