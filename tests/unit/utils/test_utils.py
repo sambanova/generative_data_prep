3ce1617f934a83368360aa32e4fa0bac18fd4a4b
Zoltan Csaki <zoltan.csaki@sambanovasystems.com>
2024-04-29 21:57:55 -0700

    Update test cases to work with new get_config_file_path

--- tests/unit/utils/test_utils.py
+++ tests/unit/utils/test_utils.py
@@ -1,3 +1,4 @@
+import os
 from generative_data_prep.utils.utils import get_config_file_path
 
 
@@ -12,4 +13,4 @@ def test_get_config_file_path():
     assert (
         str(full_config_file_path).split("/")[-1] == expected_config_filename
     ), f"The returned file path is not {expected_config_filename}."
-    assert full_config_file_path.exists(), f"The returned file path {full_config_file_path} does not exist."
+    assert os.path.exists(full_config_file_path), f"The returned file path {full_config_file_path} does not exist."
