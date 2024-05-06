121816f2c7d5e4eb7ad4eaac567e1124d7d242c2
Zoltan Csaki <137233191+snova-zoltanc@users.noreply.github.com>
2024-05-06 15:59:09 -0700

    Clean up final print statements with training requirements (#95)
    * Update logger.py
    
    * Update so that <= 120 characters
    
    * running pre-commit

--- generative_data_prep/utils/logger.py
+++ generative_data_prep/utils/logger.py
@@ -95,12 +95,14 @@ def log_elapsed_time():
 def log_training_details(dataset_metadata: Dict[str, Union[str, int, bool]]):
     """Log training parameters that need to be used with this dataset."""
     LOGGER.info(SEP_STR)
-    LOGGER.info("The dataset you have prepared requires you to set the following training parameters:")
+    LOGGER.info("When training, please adhere to the dataset requirements provided below:")
     LOGGER.info(f"    Max sequence length == {dataset_metadata['max_seq_length']}")
     LOGGER.info(f"    Model vocabulary size == {dataset_metadata['vocab_size']}")
     LOGGER.info(f"    Batch size <= {dataset_metadata['max_batch_size_train']}")
-    LOGGER.info(f"    Number of RDUs (data parallel workers) {dataset_metadata['number_of_training_files']}")
-    LOGGER.info(f"    Do eval: {'True' if int(dataset_metadata['number_of_dev_files']) >= 1 else 'False'}")
+    LOGGER.info(f"    Number of RDUs (data parallel workers) <= {dataset_metadata['number_of_training_files']}")
+    do_eval_true = "may be True or False"
+    do_eval_false = "must be False"
+    LOGGER.info(f"    Do eval {do_eval_true if int(dataset_metadata['number_of_dev_files']) >= 1 else do_eval_false}")
 
 
 def log_sep_str():
