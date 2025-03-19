7e474b10284b37d4ad316a771e0572c409a9294f
Varun Malyala <varun.malyala@sambanovasystems.com>
2025-03-19 19:22:18 +0530

    Ignore git sha logging in production as the the package is installed as a wheel file

--- generative_data_prep/utils/logger.py
+++ generative_data_prep/utils/logger.py
@@ -24,10 +24,11 @@ import os
 import sys
 from typing import Dict, Union
 
-import git
 
 LOGGER = logging.getLogger("generative_data_prep_logger")
 START_TIME = datetime.datetime.now()
+
+DEV = os.getenv("ENVIRONMENT", "dev") == "dev"
 
 try:
     SEP_STR = "-" * os.get_terminal_size().columns
@@ -60,6 +61,11 @@ def log_current_datetime():
 
 
 def log_git_commit_hash():
+    if not DEV:
+        LOGGER.debug("Running in production")
+        return
+
+    import git
     """Log the current git commit hash."""
     LOGGER.debug("Running Generative Data Prep repository: https://github.com/sambanova/generative_data_prep/")
     try:
